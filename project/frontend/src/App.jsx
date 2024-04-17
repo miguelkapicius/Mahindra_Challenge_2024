e20880fcfc8a280cd053cc240c85c8caeb8efb9e
Miguel Kapicius Caires <miguelkapicius@gmail.com>
2024-04-16 22:18:42 -0300

    feat{add Sidebar/SidebarItem component}

--- project/frontend/src/App.jsx
+++ project/frontend/src/App.jsx
@@ -1,11 +1,17 @@
-import Header from "./components/Header"
-
+import { SquaresFour, FlagCheckered, TrendUp, Trophy } from "@phosphor-icons/react";
+import Sidebar, { SidebarItem } from "./components/Sidebar";
 
 function App() {
-
-  return (
-    <Header title="Challenge" />
-  )
+    return (
+        <main className="text-slate-200">
+            <Sidebar>
+                <SidebarItem icon={<SquaresFour size={32} />} text="Dashboard" active/>
+                <SidebarItem icon={<FlagCheckered size={32} />} text="Races" alert/>
+                <SidebarItem icon={<TrendUp size={32} />} text="Statistics" />
+                <SidebarItem icon={<Trophy size={32} />} text="Rankings" />
+            </Sidebar>
+        </main>
+    );
 }
 
-export default App
+export default App;
---
+++ project/frontend/src/components/Sidebar.jsx
@@ -0,0 +1,101 @@
+import { useContext, createContext, useState } from "react"
+import {CaretDoubleLeft, CaretDoubleRight} from '@phosphor-icons/react'
+
+const SidebarContext = createContext()
+
+export default function Sidebar({ children }) {
+  const [expanded, setExpanded] = useState(true)
+  
+  return (
+    <aside className="h-screen w-max">
+      <nav className="h-full flex flex-col bg-zinc-900  shadow-sm">
+        <div className="p-4 pb-2 flex justify-between items-center mb-4">
+          <img
+            src="/mahindra-logo.png"
+            className={`overflow-hidden transition-all duration-500 ${
+              expanded ? "w-32" : "w-0"
+            }`}
+            alt=""
+          />
+          <button
+            onClick={() => setExpanded((curr) => !curr)}
+            className="p-1.5 rounded-lg hover:opacity-80 duration-200"
+          >
+            {expanded ? <CaretDoubleLeft size={20} /> : <CaretDoubleRight size={20} />}
+          </button>
+        </div>
+
+        <SidebarContext.Provider value={{ expanded }}>
+          <ul className="flex-1 px-3">{children}</ul>
+        </SidebarContext.Provider>
+
+        <div className="border-t flex p-3">
+          <img
+            src="https://ui-avatars.com/api/?background=c7d2fe&color=3730a3&bold=true"
+            alt=""
+            className="w-10 h-10 rounded-md"
+          />
+          <div
+            className={`
+              flex justify-between items-center
+              overflow-hidden transition-all ${expanded ? "w-52 ml-3" : "w-0"}
+          `}
+          >
+            <div className="leading-4">
+              <h4 className="font-semibold">Miguel Kapicius</h4>
+              <span className="text-xs text-slate-500">miguelkapicius@gmail.com</span>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </aside>
+  )
+}
+
+export function SidebarItem({ icon, text, active, alert }) {
+  const { expanded } = useContext(SidebarContext)
+  
+  return (
+    <li
+      className={`
+        relative flex items-center py-2 px-3 my-1
+        font-medium rounded-md cursor-pointer
+        transition-colors group
+        ${
+          active
+            ? "bg-red-900"
+            : "hover:bg-red-800"
+        }
+    `}
+    >
+      {icon}
+      <span
+        className={`overflow-hidden transition-all ${
+          expanded ? "w-52 ml-3" : "w-0"
+        }`}
+      >
+        {text}
+      </span>
+      {alert && (
+        <div
+          className={`absolute right-2 w-2 h-2 rounded bg-white animate-pulse ${
+            expanded ? "" : "top-2"
+          }`}
+        />
+      )}
+
+      {!expanded && (
+        <div
+          className={`
+          absolute left-full rounded-md px-2 py-1 ml-6
+          bg-slate-800 text-sm
+          invisible opacity-20 -translate-x-3 transition-all
+          group-hover:visible group-hover:opacity-100 group-hover:translate-x-0
+      `}
+        >
+          {text}
+        </div>
+      )}
+    </li>
+  )
+}
