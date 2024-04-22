b15e0f09108fe69d922f41413a59655df7efffb8
Miguel Kapicius Caires <miguelkapicius@gmail.com>
2024-04-22 11:05:11 -0300

    feat{add Card component to Home}

--- project/frontend/src/components/Card.jsx
+++ project/frontend/src/components/Card.jsx
@@ -1,11 +1,8 @@
 
-
-export default function Card({children, styles}) {
+export default function Card({ children, style }) {
     return (
-        <div className={`${styles}`}>
+        <div class={`block ${style} p-6 rounded-lg border bg-indigo-900 border-gray-700 hover:bg-opacity-70 duration-500`}>
             {children}
         </div>
     );
 }
-
-
--- project/frontend/src/routes/Home.jsx
+++ project/frontend/src/routes/Home.jsx
@@ -1,10 +1,18 @@
+import Card from "../components/Card";
+
 document.title = "E-Pit | Dashboard";
 
 function Home() {
     return (
         <>
             <section className="grid h-full grid-cols-12 grid-rows-12 gap-8">
-                <div class="block col-span-4 row-span-8 p-6 rounded-lg border bg-zinc-900 border-gray-700 hover:bg-opacity-50 duration-700">
+                <Card style="col-span-3 row-span-4"></Card>
+                <Card style="col-span-3 row-span-4"></Card>
+                <Card style="col-span-3 row-span-4"></Card>
+                <Card style="col-span-2 row-span-12"></Card>
+                <Card style="col-span-3 row-span-8"></Card>
+                <Card style="col-span-3 row-span-8"></Card>
+                <Card style="col-span-3 row-span-8">
                     <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
                         Mahindra Challenge 2024
                     </h5>
@@ -14,10 +22,7 @@ function Home() {
                         labore, nobis repudiandae inventore saepe nulla cum
                         laborum ut animi dolore neque debitis non odio et?
                     </p>
-                </div>
-
-                <div className="col-span-4 row-span-4"></div>
-                <div className="col-span-4 row-span-4"></div>
+                </Card>
             </section>
         </>
     );
