52a96c919f23efa3339db303636455729e733eef
Miguel Kapicius Caires <miguelkapicius@gmail.com>
2024-04-24 21:05:13 -0300

    ...

--- project/frontend/src/components/Card.jsx
+++ project/frontend/src/components/Card.jsx
@@ -1,7 +1,7 @@
 
 export default function Card({ children, style }) {
     return (
-        <div class={`block ${style} p-6 rounded-lg border bg-indigo-900 border-gray-700 hover:bg-opacity-70 duration-500`}>
+        <div class={`block ${style} p-6 rounded-lg border bg-slate-900/40 border-gray-900 hover:bg-slate-900/40 duration-300 hover:scale-105`}>
             {children}
         </div>
     );
--- project/frontend/src/routes/Home.jsx
+++ project/frontend/src/routes/Home.jsx
@@ -5,18 +5,20 @@ document.title = "E-Pit | Dashboard";
 function Home() {
     return (
         <>
-            <section className="grid h-full grid-cols-12 grid-rows-12 gap-8">
-                <Card style="col-span-3 row-span-4"></Card>
+            <section className="grid h-full grid-cols-12 grid-rows-12 gap-5">
+                <Card style="col-span-3 row-span-4">
+                    <h5 className="mb-5 text-2xl font-bold tracking-tight">Ranking</h5>
+                    <p className="text-5xl font-bold">1335</p>
+                </Card>
                 <Card style="col-span-3 row-span-4"></Card>
                 <Card style="col-span-3 row-span-4"></Card>
                 <Card style="col-span-2 row-span-12"></Card>
-                <Card style="col-span-3 row-span-8"></Card>
-                <Card style="col-span-3 row-span-8"></Card>
+                <Card style="col-span-6 row-span-8"></Card>
                 <Card style="col-span-3 row-span-8">
-                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                    <h5 className="mb-2 text-2xl font-bold tracking-tight">
                         Mahindra Challenge 2024
                     </h5>
-                    <p class="font-normal text-gray-700 dark:text-gray-400">
+                    <p className="font-normal text-gray-700 dark:text-gray-400">
                         Lorem ipsum dolor, sit amet consectetur adipisicing
                         elit. Corporis laboriosam est dicta quaerat, tempora
                         labore, nobis repudiandae inventore saepe nulla cum
