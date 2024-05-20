7959d44332ac8ce2e8ba0edc0fa8420fc92943ec
Miguel Kapicius <miguelkapicius@gmail.com>
2024-05-20 13:23:06 -0300

    fix{Card padding and Header component margin bottom}

--- project/frontend/app/src/components/Card.jsx
+++ project/frontend/app/src/components/Card.jsx
@@ -1,7 +1,7 @@
 
 export default function Card({ children, style }) {
     return (
-        <div class={`${style} bg-secoundary/40 rounded-lg duration-500 p-4`}>
+        <div class={`${style} bg-secoundary/40 rounded-lg duration-500 p-4 py-2`}>
             {children}
         </div>
     );
