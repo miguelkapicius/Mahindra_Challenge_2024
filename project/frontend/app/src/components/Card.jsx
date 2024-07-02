557a745f6bffc3d4e5fe98baeba45fed29399076
Miguel Kapicius <miguelkapicius@gmail.com>
2024-07-02 12:51:25 -0300

    reset

--- project/frontend/app/src/components/Card.jsx
+++
@@ -1,8 +0,0 @@
-
-export default function Card({ children, style }) {
-    return (
-        <div class={`${style} bg-secoundary/40 rounded-lg duration-500 px-8 py-4`}>
-            {children}
-        </div>
-    );
-}
