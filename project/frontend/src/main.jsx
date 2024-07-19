b6efa5f24b066ba535c9726648084530eb7b022d
Miguel Kapicius <miguelkapicius@gmail.com>
2024-07-19 19:26:53 -0300

    rename the routes file

--- project/frontend/src/main.jsx
+++ project/frontend/src/main.jsx
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom/client";
 import { App } from "./App";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
+import { ErrorPage } from "./routes/ErrorPage";
 
 const router = createBrowserRouter([
     {
         path: "/",
         element: <App />,
+        errorElement: <ErrorPage />
         children: [
             {
                 path: "/",
