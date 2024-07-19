e97a095fd260727b40957a4ebe89a8e3914770e5
Miguel Kapicius <miguelkapicius@gmail.com>
2024-07-19 19:53:49 -0300

    add styles and change the sidebar items with URL

--- project/frontend/src/components/Navbar.jsx
+++ project/frontend/src/components/Navbar.jsx
@@ -1,47 +1,73 @@
 import { AreaChart, Gamepad2, House, Trophy, User } from "lucide-react";
+import { Link, useLocation } from "react-router-dom";
 
 export function Navbar() {
+    const location = useLocation();
+    const activeCSS = "lg:border-l-2 lg:border-blue-600 lg:bg-gradient-to-r from-blue-500/50 to-zinc-950"
+
     return (
         <div className="fixed bottom-0 w-full px-6 h-14 bg-zinc-800 border-t border-zinc-700 lg:bg-zinc-950 lg:h-full lg:w-64 lg:border-r lg:border-zinc-800 lg:border-t-0 lg:flex lg:flex-col lg:space-y-16">
             <div className="hidden lg:block">
-                <span className="text-3xl flex items-center gap-2"><Trophy /> E-Pit</span>
+                <span className="text-3xl flex items-center gap-2">
+                    <Trophy /> E-Pit
+                </span>
             </div>
             <div className="flex items-center h-full justify-around gap-6 lg:h-max lg:flex-col lg:items-start">
-                <a
-                    href="#"
-                    className="p-2 size-max flex justify-center hover:text-blue-300 gap-4 lg:w-full lg:justify-start lg:border-l-2 lg:border-blue-600 lg:bg-gradient-to-r from-blue-500/50 to-zinc-950"
+                <Link
+                    to={"/"}
+                    className={`p-2 size-max flex justify-center gap-4 lg:w-full lg:justify-start ${
+                        location.pathname === "/"
+                            ? `${activeCSS}`
+                            : ""
+                    }`}
                 >
                     <House />
                     <span className="hidden lg:block">Home</span>
-                </a>
-                <a
-                    href="#"
-                    className="p-2 size-max flex justify-center hover:text-blue-300 gap-4 lg:w-full lg:justify-start"
+                </Link>
+                <Link
+                    to={"/fantasy"}
+                    className={`p-2 size-max flex justify-center gap-4 lg:w-full lg:justify-start ${
+                        location.pathname === "/fantasy"
+                            ? `${activeCSS}`
+                            : ""
+                    }`}
                 >
                     <Gamepad2 />
                     <span className="hidden lg:block">Fantasy</span>
-                </a>
-                <a
-                    href="#"
-                    className="p-2 size-max flex justify-center hover:text-blue-300 gap-4 lg:w-full lg:justify-start"
+                </Link>
+                <Link
+                    to={"/rankings"}
+                    className={`p-2 size-max flex justify-center gap-4 lg:w-full lg:justify-start ${
+                        location.pathname === "/rankings"
+                            ? `${activeCSS}`
+                            : ""
+                    }`}
                 >
                     <Trophy />
                     <span className="hidden lg:block">Rankings</span>
-                </a>
-                <a
-                    href="#"
-                    className="p-2 size-max flex justify-center hover:text-blue-300 gap-4 lg:w-full lg:justify-start"
+                </Link>
+                <Link
+                    to={"/statistics"}
+                    className={`p-2 size-max flex justify-center gap-4 lg:w-full lg:justify-start ${
+                        location.pathname === "/statistics"
+                            ? `${activeCSS}`
+                            : ""
+                    }`}
                 >
                     <AreaChart />
                     <span className="hidden lg:block">Statistics</span>
-                </a>
-                <a
-                    href="#"
-                    className="p-2 size-max flex justify-center hover:text-blue-300 gap-4 lg:w-full lg:justify-start"
+                </Link>
+                <Link
+                    to={"/profile"}
+                    className={`p-2 size-max flex justify-center gap-4 lg:w-full lg:justify-start ${
+                        location.pathname === "/profile"
+                            ? `${activeCSS}`
+                            : ""
+                    }`}
                 >
                     <User />
                     <span className="hidden lg:block">Profile</span>
-                </a>
+                </Link>
             </div>
         </div>
     );
--- project/frontend/src/main.jsx
+++ project/frontend/src/main.jsx
@@ -4,15 +4,37 @@ import { App } from "./App";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 import { ErrorPage } from "./routes/ErrorPage";
+import { HomePage } from "./routes/HomePage";
+import { StatisticsPage } from "./routes/StatisticsPage";
+import { FantasyPage } from "./routes/FantasyPage";
+import { RankingsPage } from "./routes/RankingsPage";
+import { ProfilePage } from "./routes/ProfilePage";
 
 const router = createBrowserRouter([
     {
         path: "/",
         element: <App />,
-        errorElement: <ErrorPage />
+        errorElement: <ErrorPage />,
         children: [
             {
                 path: "/",
+                element: <HomePage />
+            },
+            {
+              path: "/statistics",
+              element: <StatisticsPage />
+            },
+            {
+              path: "/fantasy",
+              element: <FantasyPage />
+            },
+            {
+              path: "/rankings",
+              element: <RankingsPage />
+            },
+            {
+              path: "/profile",
+              element: <ProfilePage />
             },
         ],
     },
