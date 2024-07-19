88b4c5c8efc0d65be970715b9f9e4b2804b85fee
Miguel Kapicius <miguelkapicius@gmail.com>
2024-07-19 19:24:50 -0300

    rename the routes folder

--- project/frontend/src/App.jsx
+++ project/frontend/src/App.jsx
@@ -1,6 +1,5 @@
 import { Outlet } from "react-router-dom";
 import { Navbar } from "./components/Navbar";
-import { RankingTable } from "./components/Rankings/RankingTable";
 
 export function App() {
     return (
--- project/frontend/src/main.jsx
+++ project/frontend/src/main.jsx
@@ -1,27 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App";
-import {
-    createBrowserRouter,
-    RouterProvider,
-  } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 
 const router = createBrowserRouter([
     {
-      path: "/",
-      element: <App />,
-      children: [
-        {
-            path: "/",
-            element: <
-        }
-      ]
+        path: "/",
+        element: <App />,
+        children: [
+            {
+                path: "/",
+            },
+        ],
     },
-  ]);
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <App />
+        <RouterProvider router={router} />
     </React.StrictMode>
 );
