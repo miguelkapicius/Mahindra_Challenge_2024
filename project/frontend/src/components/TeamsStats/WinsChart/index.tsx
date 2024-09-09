9ea5463c15030b0ee9cfb24ed3e664a9122f33ed
Miguel Kapicius <miguelkapicius@gmail.com>
2024-09-09 15:37:24 -0300

    ...

---
+++ project/frontend/src/components/TeamsStats/WinsChart/index.tsx
@@ -0,0 +1,82 @@
+
+import { CartesianGrid, Line, LineChart, XAxis } from "recharts"
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import {
+  ChartConfig,
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "@/components/ui/chart"
+
+export const description = "A line chart"
+
+const chartData = [
+  { month: "January", desktop: 186 },
+  { month: "February", desktop: 305 },
+  { month: "March", desktop: 237 },
+  { month: "April", desktop: 73 },
+  { month: "May", desktop: 209 },
+  { month: "June", desktop: 214 },
+]
+
+const chartConfig = {
+  desktop: {
+    label: "Desktop",
+    color: "hsl(var(--chart-1))",
+  },
+} satisfies ChartConfig
+
+export function WinsChart() {
+  return (
+    <Card className="w-full">
+      <CardHeader>
+        <CardTitle>Wins Rate</CardTitle>
+        <CardDescription>January - June 2024</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <ChartContainer config={chartConfig}>
+          <LineChart
+            accessibilityLayer
+            data={chartData}
+            margin={{
+              left: 12,
+              right: 12,
+            }}
+          >
+            <CartesianGrid vertical={false} />
+            <XAxis
+              dataKey="month"
+              tickLine={false}
+              axisLine={false}
+              tickMargin={8}
+              tickFormatter={(value) => value.slice(0, 3)}
+            />
+            <ChartTooltip
+              cursor={false}
+              content={<ChartTooltipContent hideLabel />}
+            />
+            <Line
+              dataKey="desktop"
+              type="natural"
+              stroke="var(--color-desktop)"
+              strokeWidth={2}
+              dot={false}
+            />
+          </LineChart>
+        </ChartContainer>
+      </CardContent>
+      <CardFooter className="flex-col items-center justify-center gap-2 text-sm">
+            <span className="text-7xl">12</span>
+            <span>Total</span>
+      </CardFooter>
+    </Card>
+  )
+}
---
+++ project/frontend/src/routes/TeamStats.tsx
@@ -0,0 +1,90 @@
+import { PodiumsChart } from "@/components/TeamsStats/PodiumsChart";
+import { PointsChart } from "@/components/TeamsStats/PointsChart";
+import { WinsChart } from "@/components/TeamsStats/WinsChart";
+import { Card, CardContent} from "@/components/ui/card";
+import {
+    Table,
+    TableBody,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from "@/components/ui/table";
+
+const pilots = [
+    {
+        id: 1,
+        name: "Edoardo Mortara",
+        image: "https://static-files.formula-e.pulselive.com/drivers/84467676-4d5d-4c97-ae07-0b7520bb95ea/right/large/728b2253-b9d4-47dd-9051-e970bff2a62f.png",
+        team: "Mahindra",
+        points: 100,
+        wins: 10,
+        podiums: 30,
+        price: 100,
+    },
+    {
+        id: 2,
+        name: "Nick De Vries",
+        image: "https://static-files.formula-e.pulselive.com/drivers/84467676-4d5d-4c97-ae07-0b7520bb95ea/right/large/8bd68290-c0ad-43b6-a1ce-ee162b7d8914.png",
+        team: "Mahindra",
+        points: 80,
+        wins: 2,
+        podiums: 6,
+        price: 80,
+    },
+];
+
+export function TeamStats() {
+
+
+    return (
+        <section className="grid grid-cols-1 md:grid-cols-4 gap-6">
+            <div className="col-span-full bg-red-400 h-60 rounded drop-shadow-lg flex justify-center items-center">
+                <h2 className="text-center text-accent p-6 rounded text-5xl font-display">
+                    Mahindra
+                </h2>
+            </div>
+            <div className="col-span-full h-max flex flex-col md:flex-row gap-6">
+                <WinsChart />
+                <PodiumsChart />
+                <PointsChart />
+            </div>
+            <div className="space-y-6 col-span-full">
+                {pilots.map((pilot) => (
+                    <Card className="flex flex-col pt-6 md:flex-row items-center gap-6">
+                        <img
+                            className="object-cover bg-accent md:bg-transparent p-6 pb-0 rounded w-1/3"
+                            src={pilot.image}
+                            alt=""
+                        />
+                        <CardContent className="w-2/3 space-y-6 overflow-hidden">
+                            <h2 className="text-2xl md:text-5xl font-display">
+                                {pilot.name}
+                            </h2>
+                            <Table className="text-2xl">
+                                <TableHeader>
+                                    <TableRow>
+                                        <TableHead>Wins</TableHead>
+                                        <TableHead>Podiums</TableHead>
+                                        <TableHead className="text-right">
+                                            Points
+                                        </TableHead>
+                                    </TableRow>
+                                </TableHeader>
+                                <TableBody>
+                                    <TableRow>
+                                        <TableCell>{pilot.wins}</TableCell>
+                                        <TableCell>{pilot.podiums}</TableCell>
+                                        <TableCell className="text-right">
+                                            {pilot.points}
+                                        </TableCell>
+                                    </TableRow>
+                                </TableBody>
+                            </Table>
+                        </CardContent>
+                    </Card>
+                ))}
+            </div>
+        </section>
+    );
+}
