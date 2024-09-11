9665381d94207737aaaf481d14d15bcfd7699fe0
Miguel Kapicius <miguelkapicius@gmail.com>
2024-09-11 17:43:21 -0300

    ...

--- project/frontend/src/routes/TeamStats.tsx
+++ project/frontend/src/routes/TeamStats.tsx
@@ -10,38 +10,19 @@ import {
     TableHeader,
     TableRow,
 } from "@/components/ui/table";
-
-const pilots = [
-    {
-        id: 1,
-        name: "Edoardo Mortara",
-        image: "https://static-files.formula-e.pulselive.com/drivers/84467676-4d5d-4c97-ae07-0b7520bb95ea/right/large/728b2253-b9d4-47dd-9051-e970bff2a62f.png",
-        team: "Mahindra",
-        points: 100,
-        wins: 10,
-        podiums: 30,
-        best_lap: "08:32.34",
-        price: 100,
-    },
-    {
-        id: 2,
-        name: "Nick De Vries",
-        image: "https://static-files.formula-e.pulselive.com/drivers/84467676-4d5d-4c97-ae07-0b7520bb95ea/right/large/8bd68290-c0ad-43b6-a1ce-ee162b7d8914.png",
-        team: "Mahindra",
-        points: 80,
-        wins: 2,
-        podiums: 6,
-        best_lap: "09:23.42",
-        price: 80,
-    },
-];
+import { teams } from "./Teams";
 
 export function TeamStats() {
+    const currentTeam = teams.find(team => team.name.toLowerCase() === new URL(window.location.href).pathname.split('/').pop());
+
     return (
         <section className="grid grid-cols-1 md:grid-cols-4 gap-6">
-            <div className="col-span-full bg-red-400 h-60 rounded drop-shadow-lg flex justify-center items-center">
+            <div 
+                className="col-span-full h-60 rounded drop-shadow-lg flex justify-center items-center"
+                style={{ backgroundColor: currentTeam?.color }}
+            >
                 <h2 className="text-center text-accent p-6 rounded text-5xl font-display">
-                    Mahindra
+                    {currentTeam?.name}
                 </h2>
             </div>
             <div className="col-span-full flex flex-col md:flex-row gap-6">
@@ -50,7 +31,7 @@ export function TeamStats() {
                 <PointsChart />
             </div>
             <div className="space-y-6 col-span-full">
-                {pilots.map((pilot) => (
+                {currentTeam?.pilots.map((pilot) => (
                     <Card className="flex flex-col pt-6 md:flex-row items-center gap-6">
                         <img
                             className="object-cover bg-accent md:bg-transparent p-6 pb-0 rounded w-1/3"
--- project/frontend/src/routes/Teams.tsx
+++ project/frontend/src/routes/Teams.tsx
@@ -9,7 +9,7 @@ import {
 } from "@/components/ui/table";
 import { NavLink } from "react-router-dom";
 
-const teams = [
+export const teams = [
     {
         name: "Mahindra",
         color: "#F87171",
@@ -18,6 +18,30 @@ const teams = [
         wins: 12,
         podiums: 30,
         points: 200,
+        pilots: [
+            {
+                id: 1,
+                name: "Edoardo Mortara",
+                image: "https://static-files.formula-e.pulselive.com/drivers/84467676-4d5d-4c97-ae07-0b7520bb95ea/right/large/728b2253-b9d4-47dd-9051-e970bff2a62f.png",
+                team: "Mahindra",
+                points: 100,
+                wins: 10,
+                podiums: 30,
+                best_lap: "08:32.34",
+                price: 100,
+            },
+            {
+                id: 2,
+                name: "Nick De Vries",
+                image: "https://static-files.formula-e.pulselive.com/drivers/84467676-4d5d-4c97-ae07-0b7520bb95ea/right/large/8bd68290-c0ad-43b6-a1ce-ee162b7d8914.png",
+                team: "Mahindra",
+                points: 80,
+                wins: 2,
+                podiums: 6,
+                best_lap: "09:23.42",
+                price: 80,
+            },
+        ]
     },
     {
         name: "ERT",
@@ -27,6 +51,30 @@ const teams = [
         wins: 20,
         podiums: 60,
         points: 300,
+        pilots: [
+            {
+                id: 1,
+                name: "Dan Ticktum",
+                image: "https://static-files.formula-e.pulselive.com/drivers/84467676-4d5d-4c97-ae07-0b7520bb95ea/right/large/0bdc60a2-9884-45b3-848d-3ffecf12fd59.png",
+                team: "ERT",
+                points: 200,
+                wins: 8,
+                podiums: 25,
+                best_lap: "08:56.08",
+                price: 80,
+            },
+            {
+                id: 2,
+                name: "Sérgio Sette Câmara",
+                image: "https://static-files.formula-e.pulselive.com/drivers/84467676-4d5d-4c97-ae07-0b7520bb95ea/right/large/b768b851-5611-40be-8687-a0974e3d2754.png",
+                team: "ERT",
+                points: 90,
+                wins: 3,
+                podiums: 8,
+                best_lap: "09:45.48",
+                price: 90,
+            },
+        ]
     },
 ];
 
