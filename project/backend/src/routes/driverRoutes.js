e2056dac8d9a6d14d865bd4853f29e0593a9ca70
Miguel Kapicius <miguelkapicius@gmail.com>
2024-09-19 20:59:26 -0300

    resetei o backend

--- project/backend/src/routes/driverRoutes.js
+++
@@ -1,6 +0,0 @@
-import { Router } from "express";
-import { importDriversCSV } from "../controllers/driverController.js";
-
-export const driverRouter = Router();
-
-importDriversCSV();
--- project/backend/src/routes/teamsRoutes.js
+++
@@ -1,6 +0,0 @@
-import { Router } from "express";
-import { importTeamsCSV } from "../controllers/teamController.js";
-
-export const teamRouter = Router();
-
-importTeamsCSV();
