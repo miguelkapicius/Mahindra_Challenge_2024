c2f346226076538c66b2c0a80f91dc8333a56e1a
Miguel Kapicius <miguelkapicius@gmail.com>
2024-10-04 09:26:36 -0300

    ...

--- project/backend/src/config/database.js
+++ project/backend/src/config/database.js
@@ -1,13 +1,11 @@
 import mongoose from "mongoose";
+import "dotenv/config";
 
 export const connectDB = async () => {
     try {
-        await mongoose.connect(
-            `mongodb+srv://miguelkapicius:zhtX2qf19TQQQ9Lo@e-pit.iunc2.mongodb.net/?retryWrites=true&w=majority&appName=E-Pit/E-Pit`,
-            {
-                enableUtf8Validation: true,
-            }
-        );
+        await mongoose.connect(process.env.MONGO_URI, {
+            enableUtf8Validation: true,
+        });
         console.log("Database is running ✅");
     } catch (error) {
         console.log("Database error ⛔", error);
